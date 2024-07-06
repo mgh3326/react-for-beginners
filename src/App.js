2bcaaf012bb668f83d75d1af384d94f31f494c6e
robin <robin@watcha.com>
2024-07-07 00:45:36 +0900

    EFFECTS - Introduction

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import {useState} from "react";
 
 function App() {
+    const [counter, setCounter] = useState(0);
+    const onClick = () => setCounter((prev) => prev + 1);
+    console.log("rende r")
     return (
         <div>
             <h1 className={styles.title}>
-                Welcome back!!
+                {counter}
             </h1>
-            <Button text={"Continue"}/>
+            <button onClick={onClick}>click me</button>
         </div>
     );
 }
