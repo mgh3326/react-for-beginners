d579b8786002c33310944ec3624838fd2c0e8115
robin <robin@watcha.com>
2024-07-06 23:08:09 +0900

    Tour of CRA

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
 function App() {
     return (
-        <div>Welcome back!!</div>
+        <div>
+            <h1 className={styles.title}>
+                Welcome back!!
+            </h1>
+            <Button text={"Continue"}/>
+        </div>
     );
 }
 
